d15ef817490460798f83a67b32bc61dc488daf19
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-06-04 13:40:58 +0200

    initial commit

--- Gallery.php
+++ Gallery.php
@@ -11,6 +11,7 @@ namespace Web\Apps\Gallery;
 
 use Web\Framework\Lib\App;
 use Web\Framework\Lib\Url;
+use Web\Framework\Lib\FileIO;
 
 // portal files loaded
 define('Gallery_loaded', true);
@@ -18,12 +19,12 @@ define('Gallery_loaded', true);
 
 final class Gallery extends App
 {
-	public $init = 'load';
 	public $lang = true;
 	public $css = true;
 	public $hooks = array(
 		'integrate_delete_member' => 'Web::App::Gallery::onMemberDelete',
 	);
+
 	public $perms = array(
 		'perm' => array(
 			'manage_album',
@@ -34,19 +35,54 @@ final class Gallery extends App
 	public $config = array(
 
 		// Group: Album
-		'album_grid' => array(
-			'group' => 'album',
+		'grid' => array(
+			'group' => 'display',
 			'control' => 'select',
-			'data' => array('array', array(3,6,9,12)),
-			'default' => 12
+			'data' => array('array', array(2,3,4,6,12), 1),
+			'default' => 4
+		),
+
+		// Group Image
+		'thumbnail_use' => array(
+			'group' => 'thumbnail',
+			'control' => 'switch',
+			'default' => 1
+		),
+		'thumbnail_width' => array(
+			'group' => 'thumbnail',
+			'control' => 'number',
+			'default' => 640
+		),
+		'thumbnail_quality' => array(
+			'group' => 'thumbnail',
+			'control' => array('number', array('min' => 1, 'max' => 100)),
+			'default' => 80
 		),
 
 		// Group: paths
-		'file_uploadpath' => array(
-			'group' => 'paths',
+		'path' => array(
+			'group' => 'upload',
 			'control' => 'input',
 			'default' => '/Web/Uploads/images/Gallery'
-		)
+		),
+		'upload_mime_types' => array(
+			'group' => 'upload',
+			'control' => 'optiongroup',
+			'data' => array(
+				'array',
+				array(
+					'image/gif',
+					'image/jpeg',
+					'image/png',
+				),
+				1
+			)
+		),
+		'upload_no_overwrite' => array(
+			'group' => 'upload',
+			'control' => 'switch',
+			'default' => 0
+		),
 	);
 
 	public $routes = array(
@@ -60,9 +96,9 @@ final class Gallery extends App
 			'ctrl' => 'album',
 			'action' => 'index'
 		),
-		'album_add' => array(
+		'album_new' => array(
 			'method' => 'GET|POST',
-			'route' => '/add',
+			'route' => '/new',
 			'ctrl' => 'album',
 			'action' => 'edit'
 		),
@@ -72,94 +108,142 @@ final class Gallery extends App
 			'ctrl' => 'album',
 			'action' => 'edit'
 		),
+		'album_delete' => array(
+			'method' => 'GET',
+			'route' => '[i:id_album]/delete',
+			'ctrl' => 'album',
+			'action' => 'delete'
+		),
 		'picture' => array(
 			'route' => '/picture/[i:id_picture]',
 			'ctrl' => 'picture',
 			'action' => 'index'
+		),
+		'picture_edit' => array(
+			'route' => '/picture/[i:id_picture]/edit',
+			'ctrl' => 'picture',
+			'action' => 'edit'
 		),
 		'picture_random' => array(
 			'route' => '/picture/random',
 			'ctrl' => 'picture',
 			'action' => 'random',
 		),
+		'picture_upload' => array(
+			'method' => 'GET|POST',
+			'route' => '/upload/[i:id_album]?',
+			'ctrl' => 'picture',
+			'action' => 'upload',
+		),
 	);
 
 	protected function initPaths()
 	{
-		global $boarddir, $boardurl;
-
 		parent::initPaths();
 
 		// fileupload dir and url
-		$this->Cfg('dir_gallery', $boarddir . $this->Cfg('file_uploadpath'));
-		$this->Cfg('url_gallery', $boardurl . $this->Cfg('file_uploadpath'));
+		$this->cfg('dir_gallery_upload', BOARDDIR . $this->cfg('path'));
+		$this->cfg('url_gallery_upload', BOARDURL . $this->cfg('path'));
 	}
 
 	public function onBefore()
 	{
-		$html = '<div id="gallery">';
-		return $html;
+		return '<div id="gallery">';
 	}
 
 	public function onAfter()
 	{
-		$html = '</div>';
-		return $html;
+		return '</div>';
 	}
 
 	/*
 	 * Creates the arrayelements of Raidmanager menu.
 	 */
-	public function Menu(&$menu_buttons)
+	public function addMenuButtons(&$menu_buttons)
 	{
-		$url = Url::factory('gallery_album_index')->getUrl();
+		// Load the list of accessible albums
+		$album_list = $this->getModel('Album')->getAlbumList();
 
-		$menu_buttons['gallery'] = array(
-			'title' => 'Gallery',
-			'href' => $url,
-			'show' => true,
-			'sub_buttons' => array()
+		$gallery_menu_buttons = array();
+
+		if ($album_list)
+		{
+			$gallery_album_buttons = array();
+
+			foreach ($album_list as $album)
+			{
+				$gallery_album_buttons['gallery_' . FileIO::cleanFilename($album->title)] = array(
+					'title' => $album->title,
+					'href' => Url::factory('gallery_album_album', array('id_album' => $album->id_album))->getUrl(),
+					'show' => true,
+				);
+			}
+
+			$gallery_menu_buttons['gallery_album_list'] = array(
+				'title' => $this->txt('album'),
+				'href' => Url::factory('gallery_album_index')->getUrl(),
+				'show' => true,
+				'sub_buttons' => $gallery_album_buttons,
+			);
+		}
+
+		$gallery_menu_buttons['gallery_upload'] = array(
+			'title' => $this->txt('upload'),
+			'href' => Url::factory('gallery_picture_upload')->getUrl(),
+			'show' => $this->checkAccess('allow_upload')
+		);
+		$gallery_menu_buttons['gallery_new'] = array(
+			'title' => $this->txt('album_new'),
+			'href' => Url::factory('gallery_album_new')->getUrl(),
+			'show' => $this->checkAccess('gallery_manage_album')
+		);
+		$gallery_menu_buttons['gallery_config'] = array(
+			'title' => $this->txt('web_config'),
+			'href' => Url::factory('admin_app_config', array('app_name' => 'gallery'))->getUrl(),
+			'show' => $this->checkAccess('gallery_manage_album')
 		);
 
-		unset($url);
+		$menu_buttons['gallery'] = array(
+			'title' => $this->txt('headline'),
+			'href' => Url::factory('gallery_album_index')->getUrl(),
+			'show' => !empty($album_list) || $this->checkAccess('gallery_manage_album'),
+			'sub_buttons' => $gallery_menu_buttons
+		);
 	}
 
 	/**
 	 * Writes membername to the picture and gallery tables of the member to delete
-	 * @param unknown $id_member
+	 * @param int $id_member
 	 */
 	public function onMemberDelete($id_member)
 	{
 		// Get the member name by member id
-		$Model = App::create('Forum')->Model('Members');
-		$Model->Find($id_member, array('member_name', 'real_name'));
+		$model = App::getInstance('forum')->getModel('members');
+		$model->find($id_member, array('member_name', 'real_name'));
 		$member_name = $member->real_name ? $member->real_name : $member->member_name;
 
 		// Create Gallery app
-		$App = App::create('Gallery');
+		$app = App::create('Gallery');
 
 		// Update the albums of this member
-		$Model = $App->Model('Album');
-		$Model->setField('member_name');
-		$Model->setFilter('id_member={int:id_member}');
-		$Model->setParameter(array(
+		$model = $app->getModel('Album');
+		$model->setField('member_name');
+		$model->setFilter('id_member={int:id_member}');
+		$model->setParameter(array(
 			'membername' => $member_name,
 			'id_member' => $id_member
 		));
-		$Model->Update();
+		$model->update();
 
 		// And then the pictures
-		$Model = $App->Model('Picture');
-		$Model->setField('member_name');
-		$Model->setFilter('id_member={int:id_member}');
-		$Model->setParameter(array(
+		$model = $app->getModel('Picture');
+		$model->setField('member_name');
+		$model->setFilter('id_member={int:id_member}');
+		$model->setParameter(array(
 			'membername' => $member_name,
 			'id_member' => $id_member
 		));
-		$Model->Update();
-
-		// Some cleanups
-		unset($App, $Model);
+		$model->update();
 	}
 }
 ?>
