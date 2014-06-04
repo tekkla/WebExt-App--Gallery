900cead3c401872befa0331b96d5cd07a764ddc4
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-06-04 15:49:19 +0200

    add language files english and german

---
+++ Language/Gallery.english.php
@@ -0,0 +1,53 @@
+<?php
+// Version: 2.0; Gallery
+
+$txt['app_gallery_name'] = 'Bildergalerien';
+
+$txt['permissiongroup_gallery_classic_perm'] = 'App: Galerie administrieren';
+$txt['permissiongroup_simple_gallery_simple_perm'] = 'App: Galerie administrieren';
+$txt['permissionname_gallery_perm_manage_album'] = 'Alben managen';
+$txt['permissionhelp_gallery_perm_manage_album'] = 'Gestattet das Anlegen, Anpassen und Löschen von Alben.';
+$txt['permissionname_gallery_perm_manage_image'] = 'Bilder managen';
+$txt['permissionhelp_gallery_perm_manage_image'] = 'Gestattet das die Verwaltung von Bildern mit den Rechten ihrew Daten zu verändern oder auch ganze Bilder zu löschen.';
+
+
+$txt['app_gallery_headline'] = 'Bildergalerien';
+$txt['app_gallery_intro'] = 'Die Bildergalerie ist Teil unseres Communitysystems. Einige Galerien sind nur unseren Raid-/Gildenmitgliedern für Uploads vorbehalten, andere hingegen können von allen registrierten Benutzen mit Bildern gefüttert werden. Wir freuen uns über jedes neue Bild und wünschen mit diesem Communitymodul viel Spass!';
+$txt['app_gallery_legal'] = '<span class="strong">Rechtlicher Hinweis:</span> Sollten Rechte Dritter durch die hier veröffentlichten Bilder verletzt werden, so liegt dies nicht in unserem Interesse. Wir kontrollieren in regelmäßigen Abständen die Galerien auf ihren Inhalt, sind aber auch nicht 24/7 dabei auf nicht zulässiges Material zu lauern. Wenn etwas nicht passen sollte, dann unter der im Impressum angegebenen Kontaktmöglichkeit melden und wir kümmern uns umgehend darum. Anstößige Bilder, Bilder mit verbotenen Motiven oder Bilder mit pornografischem Inhalt werden von uns kommentarlos gelöscht und der Useraccount des Uploaders gebannt!';
+$txt['app_gallery_pictures'] = 'Bilder';
+$txt['app_gallery_nopics'] = 'Keine Bilder in dieser Galerie vorhanden.';
+$txt['app_gallery_without_title'] = '<ohne Titel>';
+$txt['app_gallery_rnd_image'] = 'Zufallsbild';
+$txt['app_gallery_title'] = 'Titel';
+$txt['app_gallery_description'] = 'Beschreibung';
+$txt['app_gallery_from_gallery'] = 'Aus Galerie';
+$txt['app_gallery_picturedata'] = 'Bildinformationen';
+$txt['app_gallery_filesize'] = 'Dateigröße';
+$txt['app_gallery_uploader'] = 'Uploader';
+$txt['app_gallery_dimension'] = 'Abmessungen';
+$txt['app_gallery_date_upload'] = 'Datum/Zeit';
+$txt['app_gallery_gallerydata'] = 'Zugehörige Galerie';
+$txt['app_gallery_imgurl'] = 'URLs';
+$txt['app_gallery_imgurl_original'] = 'Orginal';
+$txt['app_gallery_imgurl_medium'] = 'Medium';
+$txt['app_gallery_imgurl_thumb'] = 'Thumb';
+
+
+$txt['app_gallery_cfg_album_headline_info'] = 'Albuminformationen';
+$txt['app_gallery_cfg_album_title'] = 'Titel';
+$txt['app_gallery_cfg_album_description'] = 'Beschreibung';
+$txt['app_gallery_cfg_album_tags'] = 'Tags';
+$txt['app_gallery_cfg_album_notes'] = 'interne Notizen';
+$txt['app_gallery_cfg_album_legalinfo'] = 'rechtliche Hinweise';
+
+
+$txt['app_gallery_cfg_album_headline_access'] = 'Benutzerrechte';
+$txt['app_gallery_cfg_album_accessgroups'] = 'Album ansehen';
+
+$txt['app_gallery_cfg_album_uploadgroups'] = 'Bilder hochladen';
+
+$txt['app_gallery_cfg_album_headline_options'] = 'Optionen';
+$txt['app_gallery_cfg_album_anonymous'] = 'Anonyme Bilder';
+$txt['app_gallery_cfg_album_scoring'] = 'Scoring aktiv?';
+$txt['app_gallery_cfg_album_img_per_user'] = 'Bilder pro User';
+?>
